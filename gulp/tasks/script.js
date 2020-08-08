adc6761299a9fec767ff66c78cf4eaca63dd3070
Marodina <marusea_rod@list.ru>
2020-08-08 11:22:22 +0300

    finish filter, start sort

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -3,9 +3,12 @@ const babel = require('gulp-babel');
 const terser = require('gulp-terser');
 const rename = require('gulp-rename');
 const sourcemap = require('gulp-sourcemaps');
+const eslint = require('gulp-eslint');
 
 module.exports = function script(cb) {
     return gulp.src('src/js/main.js')
+        // .pipe(eslint())
+        // .pipe(eslint.format())
         .pipe(sourcemap.init())
         .pipe(babel({
             presets: ['@babel/env']
--- src/js/main.js
+++ src/js/main.js
@@ -3,35 +3,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', (e) => {
         const target = e.target;
+        const square = document.querySelectorAll('.filter-square:checked');
+        const services = document.querySelectorAll('.filter-services:checked');
+        let priceFrom = document.querySelector('.filter-range-from');
+        let priceTo = document.querySelector('.filter-range-to');
+        const btnReset = document.querySelector('.filter__btn-reset');
+        const allCards = document.querySelectorAll('.card__item');
+        const allSortItems = document.querySelectorAll('.sort__item');
 
-        if(target.matches('#send')) {
+        if (target.matches('#send')) {
             e.preventDefault();
-            const square = document.querySelectorAll('.filter-square:checked');
-            const services = document.querySelectorAll('.filter-services:checked');
-            const priceFromValue = document.querySelector('.filter-range-from').value;
-            const priceToValue = document.querySelector('.filter-range-to').value;
-            const btnReset = document.querySelector('.filter__btn-reset');
-
-            const allCards = document.querySelectorAll('.card__item');
-
             const arrSquare = getArrFilterValue(square);
             const arrServices = getArrFilterValue(services);
-            const arrPrice = [priceFromValue, priceToValue];
 
             Array.from(allCards).forEach(card => {
                 card.classList.remove('hide');
 
                 const cardSquare = +card.querySelector('.square').getAttribute('data-square');
-                const cardServices = card.querySelector('.card-services-item').getAttribute('data-services');
                 const cardPrice = +card.querySelector('.price').getAttribute('data-price');
+                const cardServices = card.querySelectorAll('.card-services-item');
 
-                console.log(arrSquare.length );
-                console.log(arrServices.length );
+                const arrCardServices = getServicesValue(cardServices);
 
-                if(arrSquare.length || arrServices.length || priceFromValue || priceToValue) {
+                let isServices = 0;
+                arrCardServices.forEach(item => {
+                    if (arrServices.includes(item)) {
+                        isServices++
+                    }
+                });
+
+                if (arrSquare.length || arrServices.length || priceFrom.value || priceTo.value) {
                     btnReset.classList.add('show');
-                    if (!arrSquare.includes(cardSquare) && !arrServices.includes(cardServices)
-                        && (cardPrice < arrPrice[0] || cardPrice > arrPrice[1])) {
+                    if (!arrSquare.includes(cardSquare) && !arrServices.includes(cardServices) && !isServices
+                        && (cardPrice < priceFrom.value || cardPrice > priceTo.value)) {
                         card.classList.add('hide');
                     }
                 }
@@ -39,18 +43,52 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
         }
+
+        if(target.matches('#reset')) {
+            Array.from(allCards).forEach(card => {
+                card.classList.remove('hide');
+            });
+
+            clearCheckBox(square);
+            clearCheckBox(services);
+
+            priceTo.value = null;
+            priceFrom.value = null;
+
+            target.classList.remove('show');
+        }
+
+        if(target.matches('#sort')) {
+            Array.from(allSortItems).forEach(item => {
+                item.classList.add('show');
+            })
+        }
     });
+
+    const clearCheckBox = (nodeList) => {
+        Array.from(nodeList).forEach(item => {
+            item.checked = false;
+        });
+    }
+
+    const getServicesValue = (nodeList) => {
+        const arr = [];
+        Array.from(nodeList).forEach((elem) => {
+            arr.push(elem.getAttribute('data-services'));
+        });
+        return arr;
+    }
 
     const getArrFilterValue = (nodeList) => {
         const arr = [];
         Array.from(nodeList).forEach((elem) => {
-            if(isNaN(elem.value)) {
+            if (isNaN(elem.value)) {
                 arr.push(elem.value);
             } else {
                 arr.push(+elem.value);
             }
         });
-
         return arr;
     }
-});
+
+})
